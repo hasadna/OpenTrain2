f669225ec60beb19e96f005058c84e901ba71951
Eran Keydar <ekeydar@gmail.com>
2015-09-17 20:20:54 +0300

    fix json error

--- server/data/views.py
+++ server/data/views.py
@@ -14,10 +14,15 @@ def index(request):
 def submit_network(request):
     if request.method != 'POST':
         return JsonResponse({'details': 'not allowed'}, status=405)
-
-    d = json.loads(request.body)
+    try:
+        d = json.loads(request.body.decode('utf-8'))
+    except ValueError as e:
+        return JsonResponse({'details': str(e)}, status=400)
     bssid = d.get('bssid')
     name = d.get('name')
+    if bssid is None or name is None:
+        return JsonResponse({'details': 'bssid and name are mandatory'}, status=400)
+
     Network.objects.create(bssid=bssid, name=name)
 
     return JsonResponse(data={'bssid': bssid, 'name': name}, status=201)
